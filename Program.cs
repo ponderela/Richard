d3276b508d96b46cdc861b00a4583cf0a9105e07
pragasitcha <pragasit@etda.or.th>
2017-11-17 12:50:16 +0700

    .net core initialize

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Richard
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
